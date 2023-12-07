dd6a3f6002c23b85ed307e0bb59e3e0495b8e503
Zichenglong Wang <zw84@illinois.edu>
2023-12-07 17:51:00 -0600

    homepage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MusicContext } from '../Components/MusicProvider'; // Import MusicProvider
-
+import useAuthRedirect from '../Hooks/useAuthRedirect';
 import MusicPlayerBar from '../Components/MusicPlayerBar';
 import ArtistCard from '../Components/ArtistCard';
 import '../StyleSheets/Home.css';
+import InputSubmit from "../Components/InputSubmit";
+import LikeRecent from '../Components/LikeRecent';
 
 function HomePage() {
     const [query, setQuery] = useState('');
@@ -19,34 +21,34 @@ function HomePage() {
 
 
 
-    const fetchSearchResults = async () => {
-        const baseUrl = 'http://localhost:8080/api/search';
-        const searchQuery = query || '';
+    // const fetchSearchResults = async () => {
+    //     const baseUrl = 'http://localhost:8080/api/search';
+    //     const searchQuery = query || '';
     
-        // Create an object to hold the parameters
-        const queryParams = {
-            query: searchQuery,
-        };
+    //     // Create an object to hold the parameters
+    //     const queryParams = {
+    //         query: searchQuery,
+    //     };
     
         
-        // Convert the object to a URL-encoded query string
-        const queryString = new URLSearchParams(queryParams).toString();
+    //     // Convert the object to a URL-encoded query string
+    //     const queryString = new URLSearchParams(queryParams).toString();
     
-        // Construct the full URL with the query string
-        const url = `${baseUrl}?${queryString}`;
+    //     // Construct the full URL with the query string
+    //     const url = `${baseUrl}?${queryString}`;
     
-        console.log('Fetching search results from:', url);
+    //     console.log('Fetching search results from:', url);
     
-        try {
-            const response = await axios.get(url);
-            const query_data = response.data.result
-            console.log('Server Response:', query_data);
-            // console.log('Server Response type:', typeof(response.data.data[0]));
-            setSearchResults(query_data);
-        } catch (error) {
-            console.error('Search Error:', error);
-        }
-    };
+    //     try {
+    //         const response = await axios.get(url);
+    //         const query_data = response.data.result
+    //         console.log('Server Response:', query_data);
+    //         // console.log('Server Response type:', typeof(response.data.data[0]));
+    //         setSearchResults(query_data);
+    //     } catch (error) {
+    //         console.error('Search Error:', error);
+    //     }
+    // };
     
                 
     const navigate = useNavigate();
@@ -59,8 +61,6 @@ function HomePage() {
         <div className='HomePage'>
             <div className="UserPanel">
                 <div className='homeControlDiv'>
-                    
-                    <UserProfile />
                     <InputSubmit onSubmit={setQuery} />
                     <LikeRecent
                         userID={0}
